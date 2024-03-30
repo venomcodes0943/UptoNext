c4ae2d097cdbd41092accd2d54c14887010d744a
venomcodes0943 <132493450+venomcodes0943@users.noreply.github.com>
2024-03-30 12:12:21 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-3xl">Hello Welcome To Next Js</div>
+    </main>
+  );
+}
+
+export default Home;
